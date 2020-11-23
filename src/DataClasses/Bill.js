e5896c943fa2f91384bf5c0c2ee5a1e352061660
Juliette Deguillaume <Juliette.Deguillaume@bordeaux-inp.fr>
2020-11-23 10:57:42 +0100

    Adding function to check if bill is correct before processing total

--- src/DataClasses/Bill.js
+++ src/DataClasses/Bill.js
@@ -6,17 +6,44 @@ class Bill {
         this.processTotal()
     }
 
+    isBillCorrect() {
+        let billIsUndefined = (this.prices == undefined || this.quantities == undefined)
+
+        if (billIsUndefined === true) {
+            return false
+        } else {
+            let billHasDifferentArrayLengths = (this.prices.length != this.quantities.length)
+            let pricesHaveNegativeValue = this.prices.some(v => v < 0);
+            let quantitiesHaveNegativeValue = this.quantities.some(v => v < 0);
+
+            if (billHasDifferentArrayLengths === true || pricesHaveNegativeValue === true || quantitiesHaveNegativeValue === true) {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+
     processTotal() {
-        this.total = 0
-        this.prices.forEach((price, index) => {
-            this.total += price * this.quantities[index]
-        })
+        if (this.isBillCorrect() === true) {
+            this.total = 0
+            this.prices.forEach((price, index) => {
+                this.total += price * this.quantities[index]
+            })
+        } else {
+            this.total = -1
+        }
     }
 
     /**
      * @return bill's total in JSON format
      */
     getBillTotal() {
+        if (this.total === -1) {
+            return {
+                error: "error in request"
+            }
+        }
         return {
             total: this.total
         }
