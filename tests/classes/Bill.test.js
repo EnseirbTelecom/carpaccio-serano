f344e106b85f10dbdd666bcbbc51897604ae48a6
jdeguillaume <52931054+jdeguillaume@users.noreply.github.com>
2020-11-30 14:26:42 +0100

    Adding tests for Bill class (#9)
    Co-authored-by: Juliette Deguillaume <Juliette.Deguillaume@bordeaux-inp.fr>

--- tests/classes/Bill.test.js
+++ tests/classes/Bill.test.js
@@ -2,8 +2,56 @@
 
 const Bill = require('../../src/DataClasses/Bill.js')
 
-test('{ "prices" : [10, 20], "quantities" : [1, 2] } total returns {"total" : 50}.', () => {
+test('{"prices" : [10, 20], "quantities" : [1, 2]} total returns {"total" : 50}.', () => {
     let bill = { "prices" : [10, 20], "quantities": [1,2] }
     let tmp_bill = new Bill(bill)
     expect(tmp_bill.getBillTotal()).toStrictEqual({"total" : 50})
+})
+
+test('{"prices" : [], "quantities" : []} total returns {"total" : 0}.', () => {
+    let bill = { "prices" : [], "quantities": [] }
+    let tmp_bill = new Bill(bill)
+    expect(tmp_bill.getBillTotal()).toStrictEqual({"total" : 0})
+})
+
+test('{"prices" : [-10, 20], "quantities" : [1, 2]} total returns {"error" : "error in request"}.', () => {
+    let bill = { "prices" : [-10, 20], "quantities": [1,2] }
+    let tmp_bill = new Bill(bill)
+    expect(tmp_bill.getBillTotal()).toStrictEqual({"error" : "error in request"})
+})
+
+test('{"prices" : [10, 20], "quantities" : [-1, 2]} total returns {"error" : "error in request"}.', () => {
+    let bill = { "prices" : [10, 20], "quantities": [-1,2] }
+    let tmp_bill = new Bill(bill)
+    expect(tmp_bill.getBillTotal()).toStrictEqual({"error" : "error in request"})
+})
+
+test('{"prices" : [10, 20, 30], "quantities" : [1, 2]} total returns {"error" : "error in request"}.', () => {
+    let bill = { "prices" : [10, 20, 30], "quantities": [1,2] }
+    let tmp_bill = new Bill(bill)
+    expect(tmp_bill.getBillTotal()).toStrictEqual({"error" : "error in request"})
+})
+
+test('{"prices" : [10, 20], "quantities" : [1, 2, 3]} total returns {"error" : "error in request"}.', () => {
+    let bill = { "prices" : [10, 20], "quantities": [1,2,3] }
+    let tmp_bill = new Bill(bill)
+    expect(tmp_bill.getBillTotal()).toStrictEqual({"error" : "error in request"})
+})
+
+test('{"quantities" : [1, 2]} total returns {"error" : "error in request"}.', () => {
+    let bill = {"quantities": [1,2] }
+    let tmp_bill = new Bill(bill)
+    expect(tmp_bill.getBillTotal()).toStrictEqual({"error" : "error in request"})
+})
+
+test('{"prices" : [10,20]} total returns {"error" : "error in request"}.', () => {
+    let bill = { "prices" : [10,20]}
+    let tmp_bill = new Bill(bill)
+    expect(tmp_bill.getBillTotal()).toStrictEqual({"error" : "error in request"})
+})
+
+test('{} total returns {"error" : "error in request"}.', () => {
+    let bill = {}
+    let tmp_bill = new Bill(bill)
+    expect(tmp_bill.getBillTotal()).toStrictEqual({"error" : "error in request"})
 })
