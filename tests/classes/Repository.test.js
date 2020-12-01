cb70b29b793f6e5e21dc5cb1daebd62d7d986a8c
Juliette Deguillaume <Juliette.Deguillaume@bordeaux-inp.fr>
2020-12-01 14:15:48 +0100

    Adding tests to Repository class

--- tests/classes/Repository.test.js
+++ tests/classes/Repository.test.js
@@ -2,6 +2,11 @@
 
 const Repository = require('../../src/ServiceClasses/Repository.js')
 
-test('Empty test.', () => {
-    expect(true).toBe(true)
+test('Repository getInstance returns {"id" : "carpaccio-serano"}', () => {
+    expect(Repository.getInstance()).toEqual({"id" : "carpaccio-serano"})
+})
+
+test('Git repository name is carpaccio-serano', () => {
+    let tmp_repo = new Repository()
+    expect(tmp_repo.id).toBe('carpaccio-serano')
 })
