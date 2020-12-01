f135f118d90bee13a48716d87f181d5e22cb4f6c
jdeguillaume <52931054+jdeguillaume@users.noreply.github.com>
2020-12-01 14:43:52 +0100

    Adding tests to Repository class (#10)
    * Adding tests for Bill class
    
    * Adding tests to Repository class
    
    * Fixing linter problems + repository tests
    
    Co-authored-by: Juliette Deguillaume <Juliette.Deguillaume@bordeaux-inp.fr>

--- tests/classes/Repository.test.js
+++ tests/classes/Repository.test.js
@@ -1,7 +1,12 @@
 /* eslint-env jest */
 
-// const Repository = require('../../src/ServiceClasses/Repository.js')
+const Repository = require('../../src/ServiceClasses/Repository.js')
 
-test('Empty test.', () => {
-  expect(true).toBe(true)
+test('Repository getInstance returns {"id" : "carpaccio-serano"}', () => {
+  expect(Repository.getInstance()).toEqual({ id: 'carpaccio-serano' })
+})
+
+test('Git repository name is carpaccio-serano', () => {
+  const tmpRepo = Repository.getInstance()
+  expect(tmpRepo.id).toBe('carpaccio-serano')
 })
