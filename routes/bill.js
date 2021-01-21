32ec6791b306efa46c88d354a9ff8a19d8a327ab
EstebanEstoc <esteban.estoc@enseirb-matmeca.fr>
2021-01-21 15:35:27 +0100

    End the currency adding Close #29 Close #30

--- routes/bill.js
+++ routes/bill.js
@@ -4,22 +4,18 @@ const router = express.Router()
 
 /* GET bill page. */
 router.post('/', function (req, res, next) {
-  try {
-    const bill = new Bill(req.body)
-    bill
-      .processTotal()
-      .then(() => {
-        const totalBill = bill.getBillTotal()
-        res.send(totalBill)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  } catch (e) {
-    res.status(400).send({
-      error: e.message
+  const bill = new Bill(req.body)
+  bill
+    .processTotal()
+    .then(() => {
+      const totalBill = bill.getBillTotal()
+      res.send(totalBill)
     })
-  }
+    .catch(error => {
+      res.status(400).send({
+        error: error.message
+      })
+    })
 })
 
 module.exports = router
